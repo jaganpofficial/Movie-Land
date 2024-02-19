028592ff8044dd410da923e2e428dde8ae50255c
Jagan <jaganjnofficial@gmail.com>
2024-02-19 22:36:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React,{useEffect} from 'react';
 import './App.css';
+import searchIcon from './search.svg';
 
-function App() {
+// 
+const API_URL = 'http://www.omdbapi.com?apikey=6929e876';
+
+const App = ()=> {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+  }
+
+
+useEffect(() => {
+searchMovies('Spiderman');
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>MovieLand</h1>
+
+      <div className='search'>
+        <input 
+        placeholder='Search for movies'
+        value=""
+        onChange={() => {}}
+        />
+        <img 
+        src={searchIcon}
+        alt='search'
+        onClick={() => {}}
+        />
+      </div>
+
+      <div className='container'>
+
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
